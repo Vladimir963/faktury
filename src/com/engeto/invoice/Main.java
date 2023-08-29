a34e761a23816ce302389e719b8834ec0bbd8804
vl075648 <stiburek.vladimir@cetin.cz>
2023-08-29 13:32:34 +0200

    vylapsena faktura

--- src/com/engeto/invoice/Main.java
+++ src/com/engeto/invoice/Main.java
@@ -11,18 +11,26 @@ public class Main {
         int quantity = 3;
         double weight = 0.45;
         BigDecimal pricePerPiece = BigDecimal.valueOf(1800);
-        //LocalDate dateOfIssue = LocalDate.of(2022,4,7);
         LocalDate datumFaktury = LocalDate.of(2022, 4, 7);
+        datumFaktury = LocalDate.now();
+
+        //Dodělat časový interval slevy !!! if-now + kombinace datumů
+
         boolean isDiscounted = true;
         double totalWeight = quantity*weight;
+        String discountProcenta = "10%";
+        double discount = 0.9;
 
-        System.out.println("Celková hmotnost zásilky= "+ totalWeight+" kg");
+        System.out.println(item);
+        System.out.println("Celková hmotnost zásilky: "+ totalWeight+" kg");
         BigDecimal totalPrice = pricePerPiece.multiply(BigDecimal.valueOf(quantity));
-        totalPrice = totalPrice.add(BigDecimal.valueOf(45));
+        //totalPrice = totalPrice.add(BigDecimal.valueOf(45));
         System.out.println("Celková cena zásilky: "+ totalPrice+" Kč");
 
+        BigDecimal totalPriceDiscount = totalPrice.multiply(BigDecimal.valueOf(discount));
         if (isDiscounted) {
-            System.out.println("Sleva ANO: "+ totalPrice+" Kč");
+            System.out.println("Sleva ANO, cena po slevě " + discountProcenta + ": " + totalPriceDiscount+" Kč ");
+            System.out.println("Datum faktury:" + datumFaktury);
         }
 
         }
